93e306afbcf794a99e63c004ee64f885ebe14d79
Maximo <max1901@hotmail.com>
2021-12-01 02:07:31 -0500

    kmklkkm

--- model/Reserva.js
+++ model/Reserva.js
@@ -2,7 +2,7 @@ const {Schema, model} = require("mongoose")
 
 const ReservaSchema = new Schema({
     id_medico: {type: Schema.Types.ObjectId, ref:"Medico"},
-    id_paciente: {type: Schema.Types.ObjectId, ref: "Paciente"},
+    id_paciente: {type: String},
     id_horario: {type: Schema.Types.ObjectId, ref:"Horario"},
     fec_reserva: Date,
     estado: Boolean
